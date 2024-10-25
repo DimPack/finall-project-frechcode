bbf5b86e3b4d5c321080fbf98a5391dde43c0f72
Dima Prosto <csourgg@gmail.com>
2024-10-25 12:20:26 +0300

    add requestUser

--- server/src/middlewares/checkToken.js
+++ server/src/middlewares/checkToken.js
@@ -3,6 +3,8 @@ const CONSTANTS = require('../constants');
 const TokenError = require('../errors/TokenError');
 const userQueries =require('../controllers/queries/userQueries');
 const LogInError = require('../errors/LogInError');
+const UserNotFoundError = require('../errors/UserNotFoundError');
+const { verifyAccessToken } = require('../services');
 
 module.exports.checkAuth = async (req, res, next) => {
 
@@ -16,11 +18,17 @@ module.exports.checkAuth = async (req, res, next) => {
 
     const [, accessToken] = authorization.split(" ");
     const tokenData = await verifyAccessToken(accessToken);
+    if(!tokenData) {
+      return next(new TokenError('access token not verify'));
+    }
     const foundUser = await userQueries.findUser({ id: tokenData.userId });
+    if(!foundUser) {
+      return next(new UserNotFoundError());
+    }
     foundUser.password = undefined;
     return res.status(200).send({data: foundUser});
   } catch (err) {
-    next(new TokenError());
+    next(err);
   }
 };
 
--- server/src/router/index.js
+++ server/src/router/index.js
@@ -22,7 +22,7 @@ router.use("/auth", authRouter);
 
 // router.post('/login', validators.validateLogin, userController.login);
 
-router.post("/getUser", checkAuth);
+router.get("/getUser", checkAuth);
 
 router.use(checkAccessToken);
 
